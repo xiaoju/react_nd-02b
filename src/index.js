96f1a9c2b200769c5df791c19d7a6be4287a3b03
xiaoju <me@xiaoju.io>
2017-09-14 13:53:54 +0200

    refactor: for use of several reducers

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './css/index.css';
-import postsReducer from './reducers'
+import readableApp from './reducers'
 import { createStore } from 'redux'
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(
-  postsReducer,
+  readableApp,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,56 +1,8 @@
-import {
-  POST_FETCH,
-  POST_FLUSH
-} from '../actions'
+import { combineReducers } from 'redux'
+import postsReducer from './postsReducer'
 
-const emptyState = {
-  Posts: {}
-}
+const readableApp = combineReducers({
+  postsReducer
+})
 
-const dummyPosts = {
-  '8xf0y6ziyjabvozdd253nd': {
-    id: '8xf0y6ziyjabvozdd253nd',
-    timestamp: 1467166872634,
-    title: 'Udacity is the best place to learn React',
-    body: 'Everyone says so after all.',
-    author: 'thingtwo',
-    category: 'react',
-    voteScore: 6,
-    deleted: false
-  },
-  '6ni6ok3ym7mf1p33lnez': {
-    id: '6ni6ok3ym7mf1p33lnez',
-    timestamp: 1468479767190,
-    title: 'Learn Redux in 10 minutes!',
-    body: 'Just kidding. It takes more than 10 minutes to learn technology.',
-    author: 'thingone',
-    category: 'redux',
-    voteScore: -5,
-    deleted: false
-  },
-  'qqqqqqqqqqqqqqqqqqqq': {
-    id: 'qqqqqqqqqqqqqqqqqqqq',
-    timestamp: 1467111872111,
-    title: 'Another post',
-    body: 'This is my body',
-    author: 'xiaoju',
-    category: 'react',
-    voteScore: 1,
-    deleted: false
-  }
-}
-
-const postsReducer = (state = dummyPosts, action) => {
-  switch (action.type) {
-    case POST_FETCH:
-      return dummyPosts
-
-    case POST_FLUSH:
-      return {}
-
-    default:
-      return state
-  }
-}
-
-export default postsReducer
+export default readableApp
