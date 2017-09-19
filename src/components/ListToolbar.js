3684df34e7be705d47b5b982fc0ecc7cf85da21a
xiaoju <me@xiaoju.io>
2017-09-19 13:48:18 +0200

    refactor: select & delete buttons inactive if no post

--- src/components/ListToolbar.js
+++ src/components/ListToolbar.js
@@ -2,36 +2,64 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import {selectNonePost, selectAllPost, removePost } from '../actions/index'
+import SelectAllNoneButton from './SelectAllNoneButton'
 import { withRouter, Link } from 'react-router-dom'
 
 
 class ListToolbar extends Component {
+
+  constructor(props) {
+    super(props)
+    this.selectButton = this.selectButton.bind(this)
+    this.deleteButton = this.deleteButton.bind(this)
+  }
+
+  selectButton() {
+    switch (this.props.allIds.length) {
+      case 0:
+        return (
+        <button
+          className="button inactive_button">
+          Select All
+        </button>)
+      case this.props.SelectedIds.length:
+        return (
+        <button
+          onClick={this.props.selectNonePost}
+          className="button">
+          Select None
+        </button>)
+      default:
+        return (
+        <button
+          onClick={this.props.selectAllPost}
+          className="button">
+          Select All
+        </button>)
+    }
+  }
+
+  deleteButton() {
+    return (
+      (this.props.SelectedIds.length === 0) ?
+      <button
+        className="button inactive_button">
+        Delete Selected
+      </button> :
+      <button
+        onClick={this.props.removePost}
+        className="button">
+        Delete Selected
+      </button>
+    )
+  }
+
   render() {
     return (
       <div className='listToolbar'>
-        <Link to="/newpost" className="linkButton">New Post</Link>
-
-        <button
-          onClick={this.props.removePost}
-          className="button">
-          Delete Selected
-        </button>
-        <br />
-
-        {/* toggle button 'select all / 'select none' */}
-        {/* TODO make this button a reusable component, incl. CSS */}
-        { (this.props.SelectedIds.length === this.props.allIds.length) ?
-          <button
-            onClick={this.props.selectNonePost}
-            className="button">
-            Select None
-          </button> :
-          <button
-            onClick={this.props.selectAllPost}
-            className="button">
-            Select All
-          </button> }
-
+        <Link to="/newpost" className="newpostButton">New Post</Link>
+        {this.deleteButton()}
+        {this.selectButton()}
       </div>
     )
   }
