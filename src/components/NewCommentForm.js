7773d0fa9e98699cb66b274bbf818a4e6a50c001
xiaoju <me@xiaoju.io>
2017-10-08 20:30:21 +0200

    feat: implement newComment and editComment with just 2 reconfigurable buttons

--- src/components/NewCommentForm.js
+++ src/components/NewCommentForm.js
@@ -16,65 +16,134 @@ class NewCommentForm extends Component {
 
   constructor(props) {
     super(props)
-    this.deleteCommentButton = this.deleteCommentButton.bind(this)
-    this.editCommentButton = this.editCommentButton.bind(this)
+    this.submit_edit_Button = this.submit_edit_Button.bind(this)
+    this.cancel_delete_Button = this.cancel_delete_Button.bind(this)
 
     this.state = {
-      showEditForm: 'no'
+      startedInput: false
     }
 
   }
 
-  deleteCommentButton() {
+  cancel_delete_Button() {
     return (
-      (this.state.showEditForm === 'yes') || !this.props.commentId ?
-        <button
-          className="button inactive_button">
-          Delete Comment
-        </button>
-      :
-        <button
-          onClick={() => this.props.deleteComment(this.props.postId, this.props.commentId)}
-          className="button">
-          Delete Comment
-        </button>
-    )
-  }
-
-  editCommentButton() {
-    return (
-      this.state.showEditForm === 'yes' ?
-        <button
-          onClick={() => this.setState({showEditForm: 'no'})}
-          className="button">
-          Cancel
-        </button>
-      :
+      this.state.startedInput ?
         this.props.commentId ?
           <button
-            onClick={() => this.setState({showEditForm: 'yes'})}
-            className="button">
-            Edit Comment
+            className='button'
+            onClick={() => this.setState({startedInput: false})}
+          >
+            (D) Cancel
           </button>
         :
           <button
-            className="button inactive_button">
-            Edit Comment
+            className='button'
+            onClick={() => this.setState({startedInput: false})}
+          >
+            (B) Cancel new comment
+          </button>
+      :
+        this.props.commentId ?
+          <button
+            className='button'
+            onClick={() => this.props.deleteComment(this.props.postId, this.props.commentId)}
+            // this should also set commentId to nothing which triggers re-render
+          >
+            (C) delete
+          </button>
+        :
+          <button
+            className='button inactive_button'
+          >
+            (A) cancel/delete
           </button>
     )
-
-
   }
 
-  renderSubmitField(field) {
+  submit_edit_Button() {
     return (
-      <button
-        type={field.type}
-        className={`button ${field.meta.error ? 'inactive_button' : ''}`}
-        >Submit
-      </button>
+      this.state.startedInput ?
+        this.props.commentId ?
+          <button
+            className='button'
+            // onClick={}
+              // add here API call to update this comment, will trigger re-render and so startedInput comes  back to false}
+          >
+            (D) Confirm the edit
+          </button>
+        :
+          <button
+            className='button'
+            onClick={() => this.setState({startedInput: false})}
+            // and here should also create the new comment
+          >
+            (B) Submit new comment
+          </button>
+      :
+        this.props.commentId ?
+          <button
+            className='button inactive_button'
+          >
+            (C) Type text to edit
+          </button>
+        :
+          <button
+            className='button inactive_button'
+          >
+            (A) Submit/Edit
+          </button>
     )
   }
+
+  // cancel_delete_Button() {
+  //   return (
+  //     (this.state.showEditForm === 'yes') || !this.props.commentId ?
+  //       <button
+  //         className="button inactive_button">
+  //         Delete Comment
+  //       </button>
+  //     :
+  //       <button
+  //         onClick={() => this.props.deleteComment(this.props.postId, this.props.commentId)}
+  //         className="button">
+  //         Delete Comment
+  //       </button>
+  //   )
+  // }
+
+  // submit_edit_Button() {
+  //   return (
+  //     this.state.showEditForm === 'yes' ?
+  //       <button
+  //         onClick={() => this.setState({showEditForm: 'no'})}
+  //         className="button">
+  //         Cancel
+  //       </button>
+  //     :
+  //       this.props.commentId ?
+  //         <button
+  //           onClick={() => this.setState({showEditForm: 'yes'})}
+  //           className="button">
+  //           Edit Comment
+  //         </button>
+  //       :
+  //         <button
+  //           className="button inactive_button">
+  //           Edit Comment
+  //         </button>
+  //   )
+  // }
+
+  // renderSubmitField(field) {
+  //   return (
+  //     <button
+  //       type={field.type}
+  //       className={`button ${field.meta.error ? 'inactive_button' : ''}`}
+  //       onClick={field.onClick}
+  //       >{field.label}
+  //     </button>
+  //   )
+  // }
 
   renderField(field) {
     return (
@@ -105,6 +174,10 @@ class NewCommentForm extends Component {
     })
   }
 
+  onInputChange(values){
+    this.setState({startedInput: true})
+  }
+
   render(){
     const { handleSubmit } = this.props
     return (
@@ -113,28 +186,44 @@ class NewCommentForm extends Component {
         onSubmit={handleSubmit(this.onSubmit.bind(this))}>
           <div className='formFields'>
             <Field
-              name='author'
-              type='text'
-              placeholder='Type pseudo here'
-              component={this.renderField}
-            />
-            <Field
               name='body'
               type='textarea'
               placeholder='Type new comment here'
+              onChange={this.onInputChange.bind(this)}
+              component={this.renderField}
+            />
+            <Field
+              name='author'
+              type='text'
+              placeholder='Type pseudo here'
+              onChange={this.onInputChange.bind(this)}
               component={this.renderField}
             />
           </div>
           <div className='commentsToolbar'>
-            {this.deleteCommentButton()}
-            {this.editCommentButton()}
+            {this.cancel_delete_Button()}
+            {this.submit_edit_Button()}
 
-            <Field
+            {/* <Field
               name='submit'
               type={this.state.showEditForm === 'yes' ? 'submit' : ''}
               component={this.renderSubmitField}
-
+              label='submit'
             />
+
+            <Field
+              name='edit'
+              type={(this.state.showEditForm === 'no' && this.props.commentId) ? 'submit' : '' }
+              component={this.renderSubmitField}
+              label={this.state.showEditForm === 'yes' ? 'cancel' : 'edit'}
+              onClick={
+                this.state.showEditForm === 'yes' ?
+                () => this.setState({showEditForm: 'no'}) :
+                  this.props.commentId ?
+                  () => this.setState({showEditForm: 'yes'}) :
+                  ''
+              }
+            /> */}
 
           {/* {this.state.showEditForm === 'yes' ?
           <button className="button inactive_button">Submit</button>
@@ -149,7 +238,7 @@ class NewCommentForm extends Component {
 
 function validate(values){
   const errors  = {}
-  if (!values.body || !values.author) {errors.submit='Not showing the submit button'}
+  if (!values.body || !values.author) {errors.body_or_author='fields not pristine'}
   if (!values.author) {errors.author='Please choose a pseudo!'}
   if (!values.body) {errors.body='Please type your comment!'}
   return errors
