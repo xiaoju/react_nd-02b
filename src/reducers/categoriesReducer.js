c09dfe49d2ee029aa30b562f5ceb1318259dff71
xiaoju <me@xiaoju.io>
2017-10-12 14:51:10 +0200

    style: clean up unused code in categoriesReducer

--- src/reducers/categoriesReducer.js
+++ src/reducers/categoriesReducer.js
@@ -3,39 +3,9 @@ import {
   SELECT_CATEGORY,
   ALL_POSTS_IN,
   CAT_POSTS_IN,
-  SINGLE_POST_IN
 } from '../actions'
 
-// Here the data as received from API:
-//  {
-//     "categories": [
-//         {
-//             "name": "react",
-//             "path": "react"
-//         },
-//         {
-//             "name": "redux",
-//             "path": "redux"
-//         },
-//         {
-//             "name": "udacity",
-//             "path": "udacity"
-//         }
-//     ]
-// }
-//  Here the format of redux state, after conversion by FETCH_CATEGORY case:
-// {
-//   'perPath': {
-//     'react': { 'name': 'react', 'path': 'react'},
-//     'redux': { 'name': 'redux', 'path': 'redux'},
-//     'udacity': { 'name': 'udacity', 'path': 'udacity'}
-//   },
-//   'allPaths': ['react', 'redux', 'udacity'],
-//   'SelectedPath': null
-// }
-
 const empty = {
-  perPath: {},
   allPaths: [],
   selected: '',
 }
@@ -46,18 +16,8 @@ const categoryReducer = (state = empty, action) => {
     case FETCH_CATEGORY:
       return {
         ...state,
-        // converting the output of API into a normalized state format:
-        perPath: action.categories.categories.reduce((acc, curr) => {acc[curr.path] = curr; return acc}, {}),
         allPaths: action.categories.categories.map((item)=>(item.name))
       }
-
-    case SINGLE_POST_IN:
-      // after creating a post, we don't change the selectedCategory,
-      // otherwise after post creation the app would push the category name
-      // into the url. Better not. For example a post in category 'udacity' was
-      // created from the 'see all categories' view. After post creating, we want back
-      // to that view, no to the /udacity one.
-      return state
 
     case SELECT_CATEGORY:
     case ALL_POSTS_IN:
