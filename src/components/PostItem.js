31e5946f9180029955b8d67eeffc8ab3f0321f72
xiaoju <me@xiaoju.io>
2017-10-19 00:45:04 +0200

    feat: inline styling of postItem

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -22,35 +22,33 @@ class PostItem extends Component {
 
   render() {
     return (
-      <div>
-        <div
-          className='title'
-          >
-            {this.props.thisPost.title}
-        </div>
-        <div className='infoLabels'>
-          <div
-            className='passiveLabels'
-            >
-            <div className='category columnContent'>{this.props.thisPost.category}</div>
-            <div className='author columnContent'>{this.props.thisPost.author}</div>
-            <div className='timestamp columnContent'>{(new Date(this.props.thisPost.timestamp)).toLocaleString()}</div>
-            <div className='commentsCount columnContent'>{this.props.commentsCount[this.props.thisPost.id]}</div>
+      <div style={{'display': 'flex', 'flex-flow': 'column nowrap'}}>
+        <div style={{'display': 'flex', 'flex-flow': 'row nowrap', 'justify-content': 'space-between'}}>
+          <div className='title' style={{'order': '1'}}> {this.props.thisPost.title} </div>
+            <div style={{'order': '2'}}>
+              <Link
+                style={{'order': '2'}}
+                className='button'
+                to={`/${this.props.selectedCategory || '_'}/${this.props.thisPost.id}`}
+                >details
+              </Link>
+              <button
+                style={{'order': '3'}}
+                onClick={() => this.props.deletePost(this.props.thisPost.id)}
+                className="button">
+                delete
+              </button>
+              <Link
+                style={{'order': '4'}}
+                to={`/editpost/${this.props.thisPost.id}`}
+                className="button">edit</Link>
           </div>
-
-          <Link
-            className='button'
-            to={`/${this.props.selectedCategory || '_'}/${this.props.thisPost.id}`}
-            >details
-          </Link>
-
-          <button
-            onClick={() => this.props.deletePost(this.props.thisPost.id)}
-            className="button">
-            delete
-          </button>
-          <Link to={`/editpost/${this.props.thisPost.id}`} className="button">edit</Link>
-
+        </div>
+        <div className='passiveLabels'>
+          <div className='category columnContent'>{this.props.thisPost.category}</div>
+          <div className='author columnContent'>{this.props.thisPost.author}</div>
+          <div className='timestamp columnContent'>{(new Date(this.props.thisPost.timestamp)).toLocaleString()}</div>
+          <div className='commentsCount columnContent'>{this.props.commentsCount[this.props.thisPost.id]}</div>
           <VoteButton
             id={this.props.thisPost.id}
             voteScore={this.props.thisPost.voteScore}
